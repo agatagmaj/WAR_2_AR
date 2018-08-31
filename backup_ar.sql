d6ca7aa269c59efff1c97ce957a004aa688330c5
agatagmaj <agatagmaj@gmail.com>
2018-08-31 16:55:29 +0200

    Zadanie 2

---
+++ backup_ar.sql
@@ -0,0 +1,114 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.5 (Ubuntu 10.5-0ubuntu0.18.04)
+-- Dumped by pg_dump version 10.5 (Ubuntu 10.5-0ubuntu0.18.04)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    username character varying(55),
+    email character varying(55),
+    hashed_password character varying(255)
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.users_id_seq OWNER TO postgres;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, username, email, hashed_password) FROM stdin;
+1	agafija	aga@onet.pl	e7zpzBFVeFJlj2vxa91dde12779e4a3b95fe78b5a86696e3e035e61b6873b1ee8aefc0fad05709c8
+\.
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
--- models/__init__.py
+++ models/__init__.py
@@ -30,20 +30,91 @@ class Users(object):
             # saving new instance using prepared statements
             sql = f"""INSERT INTO Users(username, email, hashed_password)
             VALUES('{self.username}', '{self.email}', '{self.hashed_password}') RETURNING id"""
-           # values = (self.username, self.email, self.hashed_password)
-            cursor.execute(sql)#, values)
+            # values = (self.username, self.email, self.hashed_password)
+            cursor.execute(sql)
             self.__id = cursor.fetchone()[0]  # albo cursor.fetchone()['id']
             return True
-        return False
+        else:
+            sql = f"""UPDATE Users SET username='{self.username}', email='{self.email}', hashed_password='{self.hashed_password}'
+            WHERE id='{self.id}'"""
+            cursor.execute(sql)
+            return True
+
+    @staticmethod
+    def load_user_by_id(cursor, user_id):
+        sql = f"SELECT id, username, email, hashed_password FROM users WHERE id='{user_id}'"
+
+        cursor.execute(sql)  # (user_id, ) - bo tworzymy krotkę
+        data = cursor.fetchone()
+        if data:
+            loaded_user = Users()
+            loaded_user.__id = data[0]
+            loaded_user.username = data[1]
+            loaded_user.email = data[2]
+            loaded_user.__hashed_password = data[3]
+            return loaded_user
+        else:
+            return None
+
+    @staticmethod
+    def load_all_users(cursor):
+        sql = "SELECT id, username, email, hashed_password FROM Users"
+
+        ret = []
+        cursor.execute(sql)
+        for row in cursor.fetchall():
+            loaded_user = Users()
+            loaded_user.__id = row[0]
+            loaded_user.username = row[1]
+            loaded_user.email = row[2]
+            loaded_user.__hashed_password = row[3]
+            ret.append(loaded_user)
+        return ret
+
+    def delete(self, cursor):
+        sql = "DELETE FROM Users WHERE id=%s"
+
+        cursor.execute(sql, (self.__id,))
+        self.__id = -1
+        return True
 
 
-agata = Users()
-agata.username = "agafija"
-agata.email = "aga@onet.pl"
-agata.set_password("Helenk@", generate_salt())
+if __name__ == "__main__":
+    hela = Users()
+    hela.username = "Helka"
+    hela.email = "HelaD@gmail.com"
+    hela.set_password("Helenk@2018", generate_salt())
 
-cnx = get_connection()
-cursor = cnx.cursor()
-agata.save_to_db(cursor)
-cursor.close()
-cnx.close()
+    cnx = get_connection()
+    cursor = cnx.cursor()
+
+    ### delete object
+    a = Users.load_user_by_id(cursor, 2)
+    a.delete(cursor)
+
+    cursor.close()
+    cnx.close()
+
+### save_to_db
+# hela.save_to_db(cursor)
+
+### load_user_by_id
+
+
+### load_all_users
+# stworzyc kursor, który tworzy słownik i wyświetla alternatywa dla wyświetlania jednego atrybutu poniżej
+# a = Users.load_all_users(cursor)
+# b = []
+# for user in a:
+#     b.append(user.username)
+# print(b)
+
+### update object
+# a = Users.load_user_by_id(cursor, 2)
+# a.username = "Paw"
+# a.email = "dab.pwl@net.pl"
+# a.save_to_db(cursor)
+
+### delete object
+# a = Users.load_user_by_id(cursor, 2)
+# a.delete(cursor)
